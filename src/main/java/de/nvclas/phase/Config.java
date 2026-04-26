239c0d14cc04a7543db92f68f163aa455ae83bd3
nvclas <74192845+nvclas@users.noreply.github.com>
2026-04-26 17:37:13 +0200

    Initial

---
+++ src/main/java/de/nvclas/phase/Config.java
@@ -0,0 +1,5 @@
+package de.nvclas.phase;
+
+public class Config {
+
+}
---
+++ src/main/java/de/nvclas/phase/Phase.java
@@ -0,0 +1,13 @@
+package de.nvclas.phase;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Phase.MODID)
+public class Phase {
+    public static final String MODID = "phase";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+}
